ddc46615b3489645ff3684f9790f1c344a051758
Anna Czinege <anna.czinege3@gmail.com>
2019-09-30 16:56:58 +0200

    Add random card to page

--- server.py
+++ server.py
@@ -6,24 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    random_list = get_random_color_and_num(create_color_list(), create_number_list())
-    return render_template('index.html', random_list=random_list)
-
-
-def create_color_list():
-    list_of_colors = ['blue', 'red', 'green', 'yellow']
-    return list_of_colors
-
-
-def create_number_list():
-    list_of_numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-    return list_of_numbers
-
-
-def get_random_color_and_num(color_list, num_list):
-    random_list = [random.choice(color_list), random.choice(num_list)]
-    return random_list
-
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
--- static/manager.js
+++ static/manager.js
@@ -1,3 +1,28 @@
 let button = document.querySelector(".btn");
-//button.addEventListener("click", getRandomCard);
+button.addEventListener("click", function () {
+    getRandomCard(createCardDeck())
+});
+let cardDeck;
 
+function createCardDeck() {
+    cardDeck = [];
+    let listOfColors = ["blue", "green", "red", "yellow"];
+    let listOfNumbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+    for (let color of listOfColors) {
+        for (let number of listOfNumbers) {
+           cardDeck.push({color: `${color}`, number: `${number}`});
+           cardDeck.push({color: `${color}`, number: `${number}`});
+        }
+    }
+    return cardDeck;
+}
+
+function getRandomCard(deck) {
+    let randomCard = deck[Math.floor(Math.random() * deck.length)];
+    let cardBox = document.querySelector(".card");
+    cardBox.setAttribute("data-color", `${randomCard.color}`);
+    cardBox.setAttribute("data-number", `${randomCard.number}`);
+    let newCard = document.createElement("img");
+    newCard.setAttribute("src", "static/css/uno.jpg");
+    cardBox.appendChild(newCard);
+}
