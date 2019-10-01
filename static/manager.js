1d3cb6a9e3bbdc26666e29aa0eb7a4ca18b452a6
Anna Czinege <anna.czinege3@gmail.com>
2019-10-01 11:44:20 +0200

    Change card-hand, card-opponent, card-stack class to id

--- static/manager.js
+++ static/manager.js
@@ -1,12 +1,12 @@
 let cardDeck = [];
 let listOfColors = ["blue", "green", "red", "yellow"];
-    let listOfNumbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-    for (let color of listOfColors) {
-        for (let number of listOfNumbers) {
-            cardDeck.push({color: `${color}`, number: `${number}`});
-            cardDeck.push({color: `${color}`, number: `${number}`});
-        }
+let listOfNumbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+for (let color of listOfColors) {
+    for (let number of listOfNumbers) {
+        cardDeck.push({color: `${color}`, number: `${number}`});
+        cardDeck.push({color: `${color}`, number: `${number}`});
     }
+}
 
 //Reset button - load game.html from start to get a new first draw
 let button = document.querySelector(".button");
@@ -18,35 +18,16 @@ button.addEventListener("click", function () {
 let gameButton = document.querySelector(".btn-game");
 gameButton.addEventListener("click", firstDraw)
 
-function firstDraw () {
-    let counter = 0;
+function firstDraw() {
     for (let i = 0; i < 5; i++) {
         styleRandomCard(getRandomCard(cardDeck), "hand");
-        counter++;
-        if (counter >= 5) {
-        }
     }
     for (let i = 0; i < 5; i++) {
         styleRandomCard(getRandomCard(cardDeck), "opponent");
-        counter++;
     }
     styleRandomCard(getRandomCard(cardDeck), "stack");
     gameButton.style.display = "none";
 }
-
-
-/*function createCardDeck() {
-    cardDeck = [];
-    let listOfColors = ["blue", "green", "red", "yellow"];
-    let listOfNumbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-    for (let color of listOfColors) {
-        for (let number of listOfNumbers) {
-            cardDeck.push({color: `${color}`, number: `${number}`});
-            cardDeck.push({color: `${color}`, number: `${number}`});
-        }
-    }
-    return cardDeck;
-}*/
 
 
 function getRandomCard(deck) {
@@ -59,7 +40,7 @@ function getRandomCard(deck) {
 }
 
 function styleRandomCard(card, player) {
-    let cardBox = document.querySelector(`.card-${player}`);
+    let cardBox = document.querySelector(`#card-${player}`);
     let newCard = document.createElement("div");
     newCard.setAttribute("data-color", `${card.color}`);
     newCard.setAttribute("data-number", `${card.number}`);
@@ -68,6 +49,14 @@ function styleRandomCard(card, player) {
     newCard.style.backgroundSize = "contain";
     newCard.style.backgroundRepeat = "no-repeat";
     newCard.style.backgroundImage = `url('static/images/cards/${card.color}-${card.number}.png')`;
+    newCard.addEventListener("click", selectCard)
     cardBox.appendChild(newCard);
 }
 
+function selectCard() {
+    let stack = document.getElementById("card-stack").querySelector('[data-color]');
+    let stackColor = stack.dataset.color;
+    let stackNumber = stack.dataset.number;
+    console.log(stack.dataset.color);
+    console.log(stack.dataset.number);
+}
